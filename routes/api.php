ec99b99ef895446e4996c35476169a020e779c4e
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-09-15 14:34:47 +0700

    Install IndoRegion Library to be use in location form

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\LocationController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/register/check', [RegisterController::class, 'check'])->name('api-register-check');
+Route::get('/provinces', [LocationController::class, 'provinces'])->name('provinces');
+Route::get('/regencies/{province_id}', [LocationController::class, 'regencies'])->name('regencies');
